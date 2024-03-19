937c0a9eef8aad14cd7f2f988a8cf7f651c71ea2
StefaniaVerduga <g00425680@atu.ie>
2024-03-19 17:25:01 +0000

    update

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -1,6 +1,8 @@
 # Author: Stefania Verduga
 # Module: Web Services and Applications
-# 
+# Write a program in python that will read a file from a repository, 
+# The program should then replace all the instances of the text "Andrew" with "Stefania". 
+# The program should then commit those changes and push the file back to the repository.
 
 import requests
 from github import Github
