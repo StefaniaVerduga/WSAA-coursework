61996e9566699245746328d2203ff35bea2f0aad
StefaniaVerduga <g00425680@atu.ie>
2024-03-19 15:11:55 +0000

    assignment 3

---
+++ assignments/assignment03-cso.py
@@ -0,0 +1,16 @@
+# Author: Stefania Verduga
+# Module: Web Services and Applications
+# Write a program that retrieves the dataset for the "exchequer account (historical series)" from the CSO, and stores it into a file called "cso.json".
+
+import requests 
+import json
+
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
+
+def getAll():
+    response = requests.get(url)
+    return response.json()
+
+if __name__ == "__main__":
+    with open("cso.json", "wt") as fp:
+        print(json.dumps(getAll()), file=fp)
